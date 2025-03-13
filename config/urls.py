b5b9dce1fb244333672f5410bb7e284dc9eb2982
gkw314 <go980314@gmail.com>
2025-03-13 11:51:59 +0900

    chapter_04 과제

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,8 @@ from django.http import Http404
 from django.shortcuts import render
 from django.urls import path, include
 from fake_db import user_db
-from todo.views import todo_list, todo_info
 from users import views as users_views
+from todo.views import todo_list, todo_info, todo_create, todo_update, todo_delete
 
 _db = user_db
 
@@ -24,11 +24,11 @@ def user_info(request, user_id):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', user_list, name='user_list'),
-    path('users/<int:user_id>/', user_info, name='user_info'),
-
     path('todo/', todo_list, name='todo_list'),
+    path('todo/create/', todo_create, name='todo_create'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
+    path('todo/<int:todo_id>/update/', todo_update, name='todo_update'),
+    path('todo/<int:todo_id>/delete/', todo_delete, name='todo_delete'),
 
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/login/', users_views.login, name='login'),
